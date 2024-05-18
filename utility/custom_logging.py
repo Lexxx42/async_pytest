2254ec79412fd3e5bfe1a828c378d8efa357f65c
Konyukhov Alexander <jesusmisha@gmail.com>
2024-05-18 14:52:24 +0300

    version 0.2.0 + added logger + added loguru to requirements + added version.txt file

--- utility/custom_logging.py
+++ utility/custom_logging.py
@@ -1,3 +1,16 @@
 """ Logging module of a project. """
+import sys
 
-# TODO: create logger here.
+from loguru import logger
+
+LOG_LEVEL = "DEBUG"
+LOG_FORMAT = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</green> | "
+    "<level>{level: <8}</level> | "
+    "<yellow>Line {line: >4} ({file}):</yellow> "
+    "<cyan>{message}</cyan>"
+)
+# removing default logger
+logger.remove()
+# adding own logger with new format
+logger.add(sys.stderr, level=LOG_LEVEL, format=LOG_FORMAT, colorize=True, backtrace=True, diagnose=True)
